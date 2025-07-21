056c8c55e8dc3485cdb3db640ab3b50fada4ae87
Psevdon1m <Valentine.oleynik@gmail.com>
2025-07-21 14:08:20 +0300

    add middleware errorhandler skelet ,signup service

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,29 @@
-import express from "express";
+import express, { Request, Response } from "express";
+
+import { body, validationResult } from "express-validator";
 
 const router = express.Router();
 
-router.post("/api/users/signup", (req, res) => {
-  res.send("Hello server");
-});
+router.post(
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("Email must be provided"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Password must be between 4 and 20 chars"),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      throw new Error("Invalid email or password");
+    }
+    const { email, password } = req.body;
+    console.log("Creating a user");
+    throw new Error("Err connecting to db");
+
+    res.send({});
+  }
+);
 
 export { router as signupRouter };

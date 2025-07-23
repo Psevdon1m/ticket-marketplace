046602f68c141ef94fe47e64859fc9ff89edba20
Psevdon1m <Valentine.oleynik@gmail.com>
2025-07-23 19:27:36 +0300

    remove not used info from mongo res model

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -20,16 +20,28 @@ interface UserDoc extends mongoose.Document {
   password: string;
 }
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    toJSON: {
+      transform(doc, ret: any) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.password;
+        delete ret.__v;
+      },
+    },
+  }
+);
 
 userSchema.pre("save", async function (done) {
   // function keyword is needed to access doc info via this keyword

78e92acf774b5308237fd0f57c8c1224f26c82d9
Psevdon1m <Valentine.oleynik@gmail.com>
2025-07-25 14:27:00 +0300

    add sign in flow and generating jwt

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,7 +1,12 @@
+import type { Request, Response } from "express";
 import express from "express";
 import { body } from "express-validator";
-import type { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+
+import { Password } from "../services/password";
+import { User } from "../models/user";
 import { validateRequest } from "../middlewares/validate-request";
+import { BadRequestError } from "../errors/bad-request-error";
 
 const router = express.Router();
 
@@ -12,7 +17,39 @@ router.post(
     body("password").trim().notEmpty().withMessage("Password is required"),
   ],
   validateRequest,
-  (req: Request, res: Response) => {}
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      //do not send any detailed info that could help malicious user to understand why request is revrted
+      throw new BadRequestError("Invalid credentials");
+    }
+
+    const passwordsMatch = await Password.compare(
+      existingUser.password,
+      password
+    );
+
+    if (!passwordsMatch) {
+      throw new BadRequestError("Invalid credentials");
+    }
+
+    const userJwt = jwt.sign(
+      {
+        id: existingUser.id,
+        email: existingUser.email,
+      },
+      process.env.JWT_KEY! //checked in index.js before app launched
+    );
+
+    //store it onn seession
+    req.session = {
+      jwt: userJwt,
+    };
+
+    return res.status(200).send(existingUser);
+  }
 );
 
 export { router as signinRouter };

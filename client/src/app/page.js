4e159870510a55a47e3995e966bccf075a5bc77b
Psevdon1m <Valentine.oleynik@gmail.com>
2025-09-01 10:17:50 +0300

    updated docker img paths

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,19 +1,11 @@
 import { apiFetch } from "@/utils/apiFetch";
 
 const LandingPage = async ({ user }) => {
-  // Fetch user data directly in the server component
-  const res = await apiFetch("auth", "/api/users/currentuser");
-  const { currentUser } = res;
+    // Fetch user data directly in the server component
+    const res = await apiFetch("auth", "/api/users/currentuser");
+    const { currentUser } = res;
 
-  return (
-    <div>
-      {currentUser ? (
-        <h1>You are signed in as {currentUser.email}</h1>
-      ) : (
-        <h1>You are not signed in</h1>
-      )}
-    </div>
-  );
+    return <div className="flex items-center justify-center min-h-screen">{currentUser ? <h1>You are signed in as {currentUser.email}</h1> : <h1>You are not signed in</h1>}</div>;
 };
 
 export default LandingPage;
